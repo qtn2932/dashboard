27710e753605166feffa1ed9deb9a295692b6345
Quan Nguyen <qnguyen303@gatech.edu>
2022-12-26 21:04:44 -0500

    added styling configs and tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './App.css'
+
 const App = () => {
     return (
         <div>
-
+            <h1 className="underline text-3xl">App</h1>
         </div>
     );
 };
